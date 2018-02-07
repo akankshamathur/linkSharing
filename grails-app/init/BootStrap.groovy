cc91932b2fa3907827c78c9f4ca68db2b33629da
akanksha <mathur.akanksha2@gmail.com>
2018-02-07 17:42:36 +0530

    PopulateDummyDataService created for populating data to tables

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,26 +1,28 @@
 class BootStrap {
     def springSecurityService
+    def populateDummyDataService
 
-    def init = {servletContext ->
-       /* Role userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
-        Role adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+    def init = { servletContext ->
+        /* Role userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
+         Role adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
 
-        User adminUser = User.findByUsername('admin') ?: new User(
-                username: 'admin',
-                password: "admin").save(failOnError: true)
+         User adminUser = User.findByUsername('admin') ?: new User(
+                 username: 'admin',
+                 password: "admin").save(failOnError: true)
 
-        if (!adminUser.authorities.contains(adminRole)) {
-            UserRole.create adminUser, adminRole
-        }
-        User user = User.findByUsername('akanksha') ?: new User(
-                username: 'akanksha',
-                password: 'akanksha').save(failOnError: true)
+         if (!adminUser.authorities.contains(adminRole)) {
+             UserRole.create adminUser, adminRole
+         }
+         User user = User.findByUsername('akanksha') ?: new User(
+                 username: 'akanksha',
+                 password: 'akanksha').save(failOnError: true)
 
-        if (!user.authorities.contains(userRole)) {
-            UserRole.create user, userRole
-        }*/
-       /* User user1 = new User(username: "admin2", password: "admin2", enabled: true).save(flush:true, failOnError : true)
-        new UserRole(user1,adminRole).save(flush:true)*/
+         if (!user.authorities.contains(userRole)) {
+             UserRole.create user, userRole
+         }*/
+        /* User user1 = new User(username: "admin2", password: "admin2", enabled: true).save(flush:true, failOnError : true)
+         new UserRole(user1,adminRole).save(flush:true)*/
+        populateDummyDataService.serviceMethod()
 
     }
     def destroy = {
---
+++ grails-app/services/linksharing/PopulateDummyDataService.groovy
@@ -0,0 +1,12 @@
+package linksharing
+
+import grails.transaction.Transactional
+
+@Transactional
+class PopulateDummyDataService {
+
+    def serviceMethod() {
+        //TODO 1: add data to all the tables
+
+    }
+}
