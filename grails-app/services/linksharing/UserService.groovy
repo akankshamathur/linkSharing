2cd481b228cadca439b655569b3a36547457e2d7
akanksha <mathur.akanksha2@gmail.com>
2018-02-09 15:59:40 +0530

    allotted ROLE_USER to newly registered user

--- grails-app/services/linksharing/UserService.groovy
+++ grails-app/services/linksharing/UserService.groovy
@@ -8,13 +8,11 @@ import grails.transaction.Transactional
 
 @Transactional
 class UserService {
-    //Role userRole = new Role(authority: 'ROLE_USER')
-    User createUser(User user) {
-        Role role = Role.findById(1)
 
-        UserRole.create(user, role, true)
-        //User.create(user, role, true)
+    def createUser(User user) {
         user.save(flush: true)
+        Role role = Role.findByAuthority('ROLE_USER')
+        UserRole.create(user,role)
     }
 }
 
